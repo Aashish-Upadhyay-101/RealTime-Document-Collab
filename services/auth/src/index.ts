f4c71d9cbd33c1cd89458939225205f589beaf24
Aashish-Upadhyay-101 <boysmall300@gmail.com>
2023-05-29 01:25:19 +0545

    nginx revery_proxy for auth server

--- services/auth/src/index.ts
+++ services/auth/src/index.ts
@@ -1,7 +1,7 @@
 import app from "./app";
 
-const port = 8080;
+const port = 8000;
 
 app.listen(port, () => {
-  console.log(`Server is running on: http://localhost:${port}`);
+  console.log("Server is running on: http://localhost:8080");
 });
