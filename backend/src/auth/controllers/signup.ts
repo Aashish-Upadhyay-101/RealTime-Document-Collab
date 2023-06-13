6a21bc05d5d99257db4af8b826bfabbac761d618
Aashish-Upadhyay-101 <boysmall300@gmail.com>
2023-06-13 10:15:31 +0545

    signup flow comments

--- backend/src/auth/controllers/signup.ts
+++ backend/src/auth/controllers/signup.ts
@@ -1,3 +1,19 @@
 import { Request, Response } from "express";
 
-export const signup = (req: Request, res: Response) => {};
+export const signup = (req: Request, res: Response) => {
+  const { email, password } = req.body;
+
+  // validate input
+
+  // check if user already exists
+
+  // hash password using best hashing algorithm
+
+  // create the user and save to database
+
+  // send verification mail
+
+  // verify the new user
+
+  // send created response
+};
