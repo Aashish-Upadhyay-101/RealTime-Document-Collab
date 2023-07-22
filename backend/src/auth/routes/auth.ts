d1090b3d9dbd02652f4c7a71805453243e542b0b
Aashish-Upadhyay-101 <boysmall300@gmail.com>
2023-07-23 00:43:26 +0545

    clean up

--- backend/src/auth/routes/auth.ts
+++ backend/src/auth/routes/auth.ts
@@ -4,9 +4,6 @@ import { signup } from "../controllers/signup";
 import { getMe } from "../controllers/me";
 import { refreshToken } from "../controllers/refresh-token";
 import { logout } from "../controllers/logout";
-import { changePassword } from "../controllers/change-password";
-import { resetPassword } from "../controllers/reset-password";
-import { verifyEmail } from "../controllers/verify-email";
 
 const router = express.Router();
 
@@ -15,8 +12,5 @@ router.post("/logout", logout);
 router.post("/login", login);
 router.post("/signup", signup);
 router.post("/refresh-token", refreshToken);
-router.post("/change-password", changePassword);
-router.post("/reset-password", resetPassword);
-router.post("/verify-email", verifyEmail);
 
 export { router as authRouter };
